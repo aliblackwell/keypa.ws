e8e163fbcb06b9475faf82763b397ea19b621fb4
Alasdair Blackwell <aliblackwell@protonmail.com>
2019-12-10 16:45:53 +0000

    Build config for XCode

---
+++ bundle.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+cp ./listener/dist/keypaws ./model/KeyPaws.mlproj/Models/KeyPaws\ 3.mlmodel ./package.json ./main.js ./cat-detected.html ./settings.html ./dist/KayPa.ws\ 0.1.0\ mac\ x64/nwjs.app/Contents/Resources/app.nw/
