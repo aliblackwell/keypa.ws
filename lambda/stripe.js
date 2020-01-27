25b8f076ac015acd836b5a37078097027fae02c4
Alasdair Blackwell <aliblackwell@protonmail.com>
2020-01-27 13:46:16 +0000

    Stripe webhook function

---
+++ lambda/stripe.js
@@ -0,0 +1,27 @@
+const express = require("express")
+const serverless = require("serverless-http")
+const app = express()
+const stripe = require("stripe")(`${process.env.STRIPE_SECRET_KEY}`)
+const endpointSecret = `${process.env.STRIPE_SIGNING_SECRET}`
+const cors = require("cors")
+app.use(cors())
+app.use(require("body-parser").raw({ type: "*/*" }))
+
+app.get("*", (req, res, next) => {
+  let sig = req.headers["stripe-signature"]
+  try {
+    let myEvent = stripe.webhooks.constructEvent(req.body, sig, endpointSecret)
+    console.log("success")
+    res.send(myEvent)
+  } catch (err) {
+    console.log("stripe webhook auth fail")
+    res.send(err)
+  }
+})
+
+// or as a promise
+const handler = serverless(app)
+module.exports.handler = async (event, context) => {
+  let myHandler = await handler(event, context)
+  return myHandler
+}
