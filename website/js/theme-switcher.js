44b5209d02cba5483f0e1f6e96f0d1896da21b71
Alasdair Blackwell <aliblackwell@protonmail.com>
2020-02-03 19:34:57 +0000

    Mobile layout and start of accessible colour

---
+++ website/js/theme-switcher.js
@@ -0,0 +1,22 @@
+function getSystemTheme() {
+  const isDarkMode = window.matchMedia("(prefers-color-scheme: dark)").matches // could change on mac auto
+  return isDarkMode ? "dark" : "light"
+}
+
+function checkMode() {
+  var mode = getSystemTheme()
+  var body = document.getElementsByTagName("body")[0]
+  if (mode === "light") {    
+    body.classList.remove("dark")
+    body.classList.add("light")
+  } else {
+    body.classList.remove("light")
+    body.classList.add("dark")
+  }
+}
+
+checkMode()
+
+setInterval(function() {
+  checkMode()
+}, 1000);
