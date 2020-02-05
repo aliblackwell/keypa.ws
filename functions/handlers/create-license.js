41bf9f4b260f75f7ef569bde04a301a8631f66ed
Alasdair Blackwell <aliblackwell@protonmail.com>
2020-02-05 18:36:33 +0000

    Make license on successful stripe webhook

--- functions/handlers/create-license.js
+++ functions/handlers/create-license.js
@@ -1,7 +1,20 @@
 const fetch = require("isomorphic-fetch")
+const crypto = require("crypto")
 const btoa = require("btoa")
 
 function CreateLicense(event, res) {
+
+  const secret = "abcdefg"
+  const hash = crypto
+    .createHmac("sha256", secret)
+    .update('magiclicense')
+    .digest("hex")
+
+  const document = {
+    "id": hash
+  }
+
+
   fetch(`https://db.keypa.ws:6984/licenses/`, {
     method: "POST", // *GET, POST, PUT, DELETE, etc.
     mode: "cors", // no-cors, cors, *same-origin
@@ -13,7 +26,7 @@ function CreateLicense(event, res) {
       Authorization: "Basic " + btoa(`aliblackwell:${process.env.KEYPAWS_BACKEND_PASSWORD}`),
       // "Content-Type": "application/x-www-form-urlencoded",
     },
-    body: JSON.stringify(event),
+    body: JSON.stringify(document),
   })
     .then(response => response.json())
     .then(data => {
