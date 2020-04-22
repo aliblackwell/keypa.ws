00dbc512d2d6cfc6678ec44484924fe8678a0cb1
Alasdair Blackwell <aliblackwell@protonmail.com>
2020-04-22 13:32:27 +0100

    Remove DMG from build

--- downloads/release.js
+++ downloads/release.js
@@ -1,12 +1,12 @@
 const fs = require("fs")
 
-const dmgStats = fs.statSync(`./downloads/KeyPaws${process.env.VERSION_NUMBER}.dmg`)
-const dmgSizeInBytes = dmgStats.size
+const zipStats = fs.statSync(`./downloads/KeyPaws${process.env.VERSION_NUMBER}.zip`)
+const zipSizeInBytes = zipStats.size
 
 function updateManifest() {
   const packageJSON = JSON.parse(fs.readFileSync("./downloads/package.json"))
   packageJSON.version = process.env.VERSION_NUMBER
-  packageJSON.packages.mac64.size = dmgSizeInBytes
+  packageJSON.packages.mac64.size = zipSizeInBytes
   packageJSON.packages.mac64.url = `https://downloads.keypa.ws/KeyPaws${process.env.VERSION_NUMBER}.zip`
   fs.writeFile("./downloads/package.json", JSON.stringify(packageJSON, null, 2), err => {
     if (err) throw err
