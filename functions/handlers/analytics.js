e6c5fc3d3c83da967aab4fccf1c1f9163358e27f
Ali Blackwell <aliblackwell@protonmail.com>
2021-03-07 21:36:16 +0000

    Disable analytics notifications

--- functions/handlers/analytics.js
+++ functions/handlers/analytics.js
@@ -3,15 +3,15 @@ const nano = require('nano')(`https://aliblackwell:${process.env.KEYPAWS_BACKEND
 const { AnalyticsNotification } = require("./mailer")
 
 async function LogEvent(req, event) {
-  try {
-    const isLive = req.body.hostName.includes("www")
-    isLive && AnalyticsNotification(event.eventType)
-    const db = isLive ? "analytics" : "dev-analytics"
-    const analyticsDb = nano.db.use(db)
-    return await analyticsDb.insert(req.body)
-  } catch (e) {
-    throw e
-  }
+    try {
+        // const isLive = req.body.hostName.includes("www")
+        // isLive && AnalyticsNotification(event.eventType)
+        const db = isLive ? "analytics" : "dev-analytics"
+        const analyticsDb = nano.db.use(db)
+        return await analyticsDb.insert(req.body)
+    } catch (e) {
+        throw e
+    }
 }
 
 module.exports = { LogEvent }
